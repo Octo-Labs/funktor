c4844b4a8641ae6e3fb8c334554976f2fb3cda2d
Jeremy Green <jeremy@octolabs.com>
2021-04-21 23:55:09 -0500

    fleshing out the middleware chain

--- lib/funktor/middleware_chain.rb
+++ lib/funktor/middleware_chain.rb
@@ -1,7 +1,62 @@
 module Funktor
   class MiddlewareChain
+    attr_reader :entries
+
+    def initialize
+      @entries = []
+    end
+
+    def remove(klass)
+      entries.delete_if { |entry| entry.klass == klass }
+    end
+
+    def add(klass, *args)
+      remove(klass)
+      entries << Entry.new(klass, *args)
+    end
+
+    def prepend(klass, *args)
+      remove(klass)
+      entries.insert(0, Entry.new(klass, *args))
+    end
+
+    def insert_before(oldklass, newklass, *args)
+      remove(newklass)
+      i = entries.index { |entry| entry.klass == oldklass } || 0
+      entries.insert(i, Entry.new(newklass, *args))
+    end
+
+    def insert_after(oldklass, newklass, *args)
+      remove(newklass)
+      i = entries.index { |entry| entry.klass == oldklass } || entries.count - 1
+      entries.insert(i + 1, Entry.new(newklass, *args))
+    end
+
     def invoke(*args)
-      return yield
+      return yield if @entries.empty?
+
+      middlewares = entries.map(&:instantiate)
+      traverse_chain = proc do
+        if middlewares.empty?
+          yield
+        else
+          middlewares.shift.call(*args, &traverse_chain)
+        end
+      end
+      traverse_chain.call
+    end
+
+    class Entry
+      attr_reader :klass
+
+      def initialize(klass, *args)
+        @klass = klass
+        @args = args
+      end
+
+      def instantiate
+        @klass.new(*@args)
+      end
     end
   end
 end
--- spec/funktor/middleware_chain_spec.rb
+++ spec/funktor/middleware_chain_spec.rb
@@ -1,10 +1,125 @@
 RSpec.describe Funktor::MiddlewareChain do
-  it 'will yield to a block when the chain is empty' do
-    chain = Funktor::MiddlewareChain.new
-    test_block_called = false
-    chain.invoke do
-      test_block_called = true
+  class TestMiddleware
+    def initialize(history_array)
+      @history_array = history_array
     end
-    expect(test_block_called).to be_truthy
+    def call(*args)
+      @history_array.push "#{self.class.name} before"
+      yield
+      @history_array.push "#{self.class.name} after"
+    end
+  end
+
+  class AnotherTestMiddleware < TestMiddleware
+  end
+
+  class PreventMiddleware
+    def initialize(history_array)
+      @history_array = history_array
+    end
+    def call(*args)
+      @history_array.push "#{self.class.name}"
+    end
+  end
+
+  describe 'add' do
+    it 'can add a middleware to the entries array' do
+      chain = Funktor::MiddlewareChain.new
+      chain.add TestMiddleware
+      expect(chain.entries.count).to eq(1)
+    end
+    it 'will not add a duplicate middleware to the entries array' do
+      chain = Funktor::MiddlewareChain.new
+      2.times{ chain.add TestMiddleware }
+      expect(chain.entries.count).to eq(1)
+    end
+  end
+
+  describe 'remove' do
+    it 'can remove a middleware from the entries array' do
+      chain = Funktor::MiddlewareChain.new
+      chain.add TestMiddleware
+      expect(chain.entries.count).to eq(1)
+      chain.remove TestMiddleware
+      expect(chain.entries.count).to eq(0)
+    end
+  end
+
+  describe 'insert_before' do
+    it 'can insert at the right place' do
+      chain = Funktor::MiddlewareChain.new
+      chain.add TestMiddleware
+      chain.add AnotherTestMiddleware
+      chain.insert_before AnotherTestMiddleware, PreventMiddleware
+      expect(chain.entries.map(&:klass)).to eq([TestMiddleware, PreventMiddleware, AnotherTestMiddleware])
+    end
+  end
+
+  describe 'insert_after' do
+    it 'can insert at the right place' do
+      chain = Funktor::MiddlewareChain.new
+      chain.add TestMiddleware
+      chain.add AnotherTestMiddleware
+      chain.insert_after TestMiddleware, PreventMiddleware
+      expect(chain.entries.map(&:klass)).to eq([TestMiddleware, PreventMiddleware, AnotherTestMiddleware])
+    end
+  end
+
+  describe 'invoke' do
+    it 'will call a middleware and then yield to a block' do
+      history = []
+      chain = Funktor::MiddlewareChain.new
+      chain.add TestMiddleware, history
+      chain.invoke do
+        history.push "test block"
+      end
+      expect(history).to eq([
+        "TestMiddleware before",
+        "test block",
+        "TestMiddleware after"
+      ])
+    end
+
+    it 'will call several middlewares and then yield to a block' do
+      history = []
+      chain = Funktor::MiddlewareChain.new
+      chain.add TestMiddleware, history
+      chain.add AnotherTestMiddleware, history
+      chain.invoke do
+        history.push "test block"
+      end
+      expect(history).to eq([
+        "TestMiddleware before",
+        "AnotherTestMiddleware before",
+        "test block",
+        "AnotherTestMiddleware after",
+        "TestMiddleware after"
+      ])
+    end
+
+    it 'will allow a middleware in the middle of the chain to prevent the yield to a block' do
+      history = []
+      chain = Funktor::MiddlewareChain.new
+      chain.add TestMiddleware, history
+      chain.add PreventMiddleware, history
+      chain.add AnotherTestMiddleware, history
+      chain.invoke do
+        history.push "test block"
+      end
+      expect(history).to eq([
+        "TestMiddleware before",
+        "PreventMiddleware",
+        "TestMiddleware after"
+      ])
+    end
+
+    it 'will yield to a block when the chain is empty' do
+      chain = Funktor::MiddlewareChain.new
+      test_block_called = false
+      chain.invoke do
+        test_block_called = true
+      end
+      expect(test_block_called).to be_truthy
+    end
   end
 end
