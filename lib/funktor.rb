e89b1a349f79e65bec221e21ee91149dbc4f648b
Jeremy Green <jeremy@octolabs.com>
2021-04-22 16:15:16 -0500

    Add middleware configs

--- lib/funktor.rb
+++ lib/funktor.rb
@@ -8,4 +8,16 @@ require 'funktor/middleware_chain'
 module Funktor
   class Error < StandardError; end
   # Your code goes here...
+
+  def self.job_pusher_middleware
+    @job_pusher_chain ||= MiddlewareChain.new
+    yield @job_pusher_chain if block_given?
+    @job_pusher_chain
+  end
+
+  def self.active_job_handler_middleware
+    @active_job_handler_chain ||= MiddlewareChain.new
+    yield @active_job_handler_chain if block_given?
+    @active_job_handler_chain
+  end
 end
--- spec/funktor_spec.rb
+++ spec/funktor_spec.rb
@@ -2,4 +2,32 @@ RSpec.describe Funktor do
   it "has a version number" do
     expect(Funktor::VERSION).not_to be nil
   end
+
+  class EmptyMiddleware; end
+
+  describe 'job_pusher_middleware' do
+    it 'returns a MiddleWareChain' do
+      expect(Funktor.job_pusher_middleware).to be_a(Funktor::MiddlewareChain)
+    end
+    it 'is persistent' do
+      expect(Funktor.job_pusher_middleware.entries.count).to eq(0)
+      Funktor.job_pusher_middleware do |chain|
+        chain.add EmptyMiddleware
+      end
+      expect(Funktor.job_pusher_middleware.entries.count).to eq(1)
+    end
+  end
+
+  describe 'active_job_handler_middleware' do
+    it 'returns a MiddleWareChain' do
+      expect(Funktor.active_job_handler_middleware).to be_a(Funktor::MiddlewareChain)
+    end
+    it 'is persistent' do
+      expect(Funktor.active_job_handler_middleware.entries.count).to eq(0)
+      Funktor.active_job_handler_middleware do |chain|
+        chain.add EmptyMiddleware
+      end
+      expect(Funktor.active_job_handler_middleware.entries.count).to eq(1)
+    end
+  end
 end
