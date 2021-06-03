17adfb8888e27e0d8da805aaf9dc9dab63b50c81
Jeremy Green <jeremy@octolabs.com>
2021-06-02 20:38:07 -0500

    This was some WIP. Kind of rethinking the 'bootstrap' idea...

--- lib/funktor/cli/bootstrap.rb
+++ lib/funktor/cli/bootstrap.rb
@@ -1,18 +1,32 @@
 module Funktor
   module CLI
     class Bootstrap < Thor::Group
+      include Thor::Actions
 
       class_option :file, :aliases => "-f",
         :type => :string, :desc => "The bootstrap file to generate.",
         :default => "funktor.yml"
+      class_option :directory, :aliases => "-d",
+        :type => :string, :desc => "The directory to initialize",
+        :default => "funktor"
 
       desc <<~DESC
         Description:
           Bootstrap a new funktor application by generating a funktor.yml file."
       DESC
 
+      def self.source_root
+        File.dirname(__FILE__)
+      end
+
       def funktor_yml
-        puts options[:file]
+        puts funktor_file_target
+        template "templates/funktor.yml", funktor_file_target
+      end
+
+      private
+      def funktor_file_target
+        File.join options[:directory], options[:file]
       end
 
     end
