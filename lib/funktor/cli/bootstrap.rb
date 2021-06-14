b49ac81096cf0d38f23a08c7e848a31f58b1dba6
Jeremy Green <jeremy@octolabs.com>
2021-06-14 17:08:47 -0500

    Name the init file more clearly

--- lib/funktor/cli/bootstrap.rb
+++ lib/funktor/cli/bootstrap.rb
@@ -7,11 +7,11 @@ module Funktor
 
       class_option :file, :aliases => "-f",
         :type => :string, :desc => "The bootstrap file to generate.",
-        :default => "funktor.yml"
+        :default => "funktor_init.yml"
 
       desc <<~DESC
         Description:
-          Bootstrap a new funktor application by generating a funktor.yml file."
+          Bootstrap a new funktor application by generating a funktor_init.yml file."
       DESC
 
       def self.source_root
@@ -21,7 +21,7 @@ module Funktor
       def funktor_yml
         # TODO - Should we camelize the app name before writing it into the config? (CloudFormation names get weird with underscores and dashes.)
         puts funktor_file_target
-        template "templates/funktor.yml", funktor_file_target
+        template "templates/funktor_init.yml", funktor_file_target
       end
 
       private
