cd9a036d7e6a66b28a69bcfaf5e0c38169ea8611
Jeremy Green <jeremy@octolabs.com>
2021-06-29 22:31:10 -0500

    An optimization for the incoming job handler

--- lib/funktor/cli/templates/lambda_event_handlers/incoming_job_handler.rb
+++ lib/funktor/cli/templates/lambda_event_handlers/incoming_job_handler.rb
@@ -1,4 +1,7 @@
-require_relative '../config/boot'
+# For this handler we don't need to know about your app, or any of the other gems,
+# so instead of doing `require_relative '../config/boog'` we just manually require
+# the one gem that we do need.
+require 'funktor'
 
 $handler = Funktor::IncomingJobHandler.new
 
