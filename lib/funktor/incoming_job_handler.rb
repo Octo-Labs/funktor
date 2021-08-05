fb31239ec11cefad7b825d506e941e09d37eb9bd
Jeremy Green <jeremy@octolabs.com>
2021-08-05 14:38:15 -0500

    push far future jobs to dynamo

--- lib/funktor/incoming_job_handler.rb
+++ lib/funktor/incoming_job_handler.rb
@@ -1,4 +1,5 @@
 require 'aws-sdk-sqs'
+require 'aws-sdk-dynamodb'
 require 'active_support/core_ext/string/inflections'
 
 module Funktor
@@ -20,11 +21,22 @@ module Funktor
       @sqs_client ||= ::Aws::SQS::Client.new
     end
 
+    def dynamodb_client
+      @dynamodb_client ||= ::Aws::DynamoDB::Client.new
+    end
+
     def dispatch(job)
       Funktor.incoming_job_handler_middleware.invoke(job) do
-        Funktor.logger.debug "pushing to work queue for delay = #{job.delay}"
-        push_to_work_queue(job)
-        @tracker.track(:queued, job)
+        # TODO : This number should be configurable via ENV var
+        if job.delay < 60 # for now we're testing with just one minute * 5 # 5 minutes
+          Funktor.logger.debug "pushing to work queue for delay = #{job.delay}"
+          push_to_work_queue(job)
+          @tracker.track(:queued, job)
+        else
+          Funktor.logger.debug "pushing to jobs table for delay = #{job.delay}"
+          push_to_jobs_table(job)
+          @tracker.track(:scheduled, job)
+        end
       end
     end
 
@@ -42,6 +54,23 @@ module Funktor
         queue_url: queue_for_job(job),
         message_body: job.to_json,
         delay_seconds: job.delay
+      })
+    end
+
+    def delayed_job_table
+      ENV['FUNKTOR_JOBS_TABLE']
+    end
+
+    def push_to_jobs_table(job)
+      perform_at = (Time.now + job.delay).utc
+      resp = dynamodb_client.put_item({
+        item: {
+          payload: job.to_json,
+          jobId: job.job_id,
+          performAt: perform_at.iso8601,
+          performAtDate: perform_at.to_date.iso8601
+        },
+        table_name: delayed_job_table
       })
     end
 
--- spec/funktor/incoming_job_handler_spec.rb
+++ spec/funktor/incoming_job_handler_spec.rb
@@ -7,8 +7,15 @@ RSpec.describe Funktor::IncomingJobHandler, type: :handler do
   let(:delay){ 0 }
   let(:single_job_event){ create_event [HelloWorker], delay }
   let(:sqs_client){ double Aws::SQS::Client }
+  let(:dynamodb_client){ double Aws::DynamoDB::Client }
   let(:incoming_job_handler) do
     Funktor::IncomingJobHandler.new()
+  end
+
+  before :each do
+    # TODO - Clean this up and really test something...
+    fake_tracker = double(Funktor::ActivityTracker, track: nil)
+    allow(Funktor::ActivityTracker).to receive(:new).and_return(fake_tracker)
   end
 
   describe 'call' do
@@ -21,9 +28,9 @@ RSpec.describe Funktor::IncomingJobHandler, type: :handler do
     end
     describe 'with a long delay' do
       let(:delay){ 1800 }
-      it 'should send a message to the work queue' do
-        expect(sqs_client).to receive(:send_message).and_return(nil)
-        expect(incoming_job_handler).to receive(:sqs_client).and_return(sqs_client)
+      it 'should save a record to DynamoDB' do
+        expect(dynamodb_client).to receive(:put_item).and_return(nil)
+        expect(incoming_job_handler).to receive(:dynamodb_client).and_return(dynamodb_client)
         incoming_job_handler.call(event: single_job_event, context: {})
       end
     end
