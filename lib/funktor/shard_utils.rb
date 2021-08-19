eea1db610220302f5891329f54a242038342f4b5
Jeremy Green <jeremy@octolabs.com>
2021-08-19 17:26:59 -0500

    Make sure shard doesn't blow up if we don't have a job id

--- lib/funktor/shard_utils.rb
+++ lib/funktor/shard_utils.rb
@@ -1,5 +1,7 @@
 module ShardUtils
   def calculate_shard(job_id)
+    # TODO : Figure out how/why we could get a missing job_id passed in here...
+    job_id ||= ""
     # TODO - Should the number of shards be configurable?
     job_id.sum % 64
   end
